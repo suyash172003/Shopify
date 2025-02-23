b5c676afa7a2a7445cf9afccc6fe8aa91fc7f6be
suyash172003 <suyash172003@gmail.com>
2025-02-23 14:30:55 +0000

    Changes for the Navbar and Landing Page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,26 +6,26 @@ function Landing() {
   const heading = ["Welcome", "to", "Apni", "Dukkan"]
   return (
     <div className='h-screen w-full flex flex-col overflow-hidden gap-5'>
-         <div className='w-full h-14 flex justify-center items-center text-black font-serif gap-10'>
-            {navbar.map((item, index) => (
-                <h1 className='font-black text-2xl text-black' key={index}>{item}</h1>
-            ))}
-        </div>
-        <div className='h-full w-full flex flex-row gap-50 justify-between items-center'>
-          <div className='h-full w-50'>
-            <div className='flex flex-col'>
-            {heading.map((item, index) => (
-              <h1 className='text-6xl text-black font-extrabold' key={index}>{item}</h1>
-            ))}
-            <button className='bg-amber-600 h-10 w-20 rounded-4xl'>Order</button>
-            </div>
+         <div className='h-10 w-full flex flex-row justify-between items-center'>
+            <ul className='h-full w-full flex flex-row justify-center items-center gap-10'>
+              {navbar.map((item, index) => {
+                return <li key={index}>{item}</li>
+              })}
+            </ul>
+         </div>
+         <div className='h-full w-full flex flex-row justify-center items-center gap-5'>
+            <div className='h-full w-1/2 flex flex-col justify-center items-center gap-5'>
+            <ul className='h-96 w-full flex flex-col gap-5 justify-center items-center'>
+              {heading.map((item, index) => {
+                return <h1 className='text-6xl font-bold text-black' key={index}>{item}</h1>
+              })}
+            </ul>
+            <button className='h-10 w-20 bg-amber-400 text-white rounded-md'>Shop Now</button>
           </div>
-          <div className='h-full w-96'>
-            <div className='px-20 flex justify-end overflow-hidden rounded-br-sm'>
-                <div className='h-96 w-96 bg-orange-200 rotate-45 -translate-y-1/3'></div>
-            </div>
+          <div className='px-40 h-full w-1/2 overflow-hidden'>
+              <div className='h-80 w-80 rotate-45 bg-amber-100'></div>
           </div>
-        </div>
+         </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import '../index.css'
 import image from "../assets/shopping-bag.png"
+import { IoIosSearch } from "react-icons/io";
 
 function Navbar() {
   const word = "SHOPIFY".split("")
   return (
     <div className='h-20 w-full flex flex-row bg-amber-100 '>
-      <div className='w-full h-full flex flex-row items-center justify-start px-10'>
+      <div className='w-1/2 h-full flex flex-row items-center justify-start px-10'>
         <img src={image} className='h-10 w-10'></img>
         <div className='text-black'>{word}</div>
       </div>
-      <div className=' flex items-center justify-end'>
-        <div className='py-4 w-50 h-10 bg-white rounded-4xl'></div>
+      <div className='w-1/2 h-full flex flex-row items-center justify-end px-10 gap-10'>
+        <div className='py-2 px-4 w-64 max-w-md bg-white rounded-full flex items-center'>
+          <input type='text' placeholder='Search' className='flex-grow h-10 bg-transparent outline-none px-4'></input>
+          <IoIosSearch className='h-6 w-6 text-gray-500'></IoIosSearch>
+        </div>
+        <button className='h-10 w-20 bg-amber-400 text-white rounded-md'>Sign In</button>
+        <button className='h-10 w-20 bg-amber-400 text-white rounded-md'>Sign Up</button>
       </div>
     </div>
   )
